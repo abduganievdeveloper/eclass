95c48458fb6e189b764170d9d40e1f997c2a01e1
AbdumalikFront <iwillstudyingermany@gmail.com>
2019-02-06 21:05:29 +0500

    fifth commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Admin } from './admin/Admin';
 import {Provider} from 'react-redux';
 import {connect} from 'react-redux';
 import {store} from './admin/store.js';
+import {BrowserRouter} from 'react-router-dom';
 class App extends Component {
  
   render() {
